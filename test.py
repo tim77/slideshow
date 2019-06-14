ef4e35c70f6697b84b25ea8cb25ab497623f2ca9
ivan-tretyak <das.blackya@gmail.com>
2019-06-14 09:00:58 +0300

    switch to new branch

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import argparse
 parser = argparse.ArgumentParser(description='Process some integers.',
 prog="my")
-parser.add_argument('--foo', help='foo help')
-parser.add_argument('-t',help='time to display')
-args = parser.parse_args[1]
-print(args)
+parser.add_argument('--foo',type=int, help='foo help')
+parser.add_argument('-t',type=int,help='time to display',metavar='N')
+parser.add_argument('--path','-p',type=str,help='path to search images')
+args = parser.parse_args()
+print(args.t)
--- test_recursive_walk.py
+++ test_recursive_walk.py
@@ -4,19 +4,13 @@ import sys
 from os import listdir
 from PIL import Image
 import datetime
+import argparse
 #Объявим глобальные переменные
 global xml_file_folder
 global watching_directory
 #Проверяем количество ключей и задаем глобальные переменные
 def check_key():
-	if len(sys.argv)<3 or len(sys.argv)%2==0:
-		help()
-	elif len(sys.argv)==3 and dir_exist(sys.argv[1]) and is_xml(sys.argv[2]):
-		print('yes')
-	elif len(sys.argv)>3 and dir_exist(sys.argv[1]) and is_xml(sys.argv[2]):
-		print('yes')
-	else:
-		help()
+	parser = argparse.ArgumentParser(description='
 def help():
 	print("We are input incorrect data. Please check this:\nslideshow /path/to/directory /path/to/xml/file [Optional parametrs]\nCheck the optional parameters(necessarily with value)\n\t--duration, -d seconds - time to change wallpaper\n\t--transition, -t time seconds - wallpaper change time\n\t--amount, -a - amount of wallpaper")
 def dir_exist(path):
